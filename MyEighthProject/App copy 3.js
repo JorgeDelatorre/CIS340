5050c3a67d0e1ef57c1b212e83884f77984bde4b
JorgeDelatorre <103400087+JorgeDelatorre@users.noreply.github.com>
2022-05-09 16:12:53 +0000

    Adding Buttons and Upload Images

---
+++ MyEighthProject/App copy 3.js
@@ -0,0 +1,61 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useState, Component } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+
+export default function App() {
+  
+  return (
+    <View style={styles.container}>
+      <Image source={{uri: 'https://raw.githubusercontent.com/AbdunabiRamadan/CIS340/master/images/logo.png' }}
+      style={styles.logo} />
+
+      <Text style={styles.insts}>
+        Press the button below to select an image on your phobe!
+      </Text>
+
+      <TouchableOpacity style={styles.button} onPress={() => alert('You have not selected an image yet')} >
+
+        <Text style={styles.buttonText}>Pick Image</Text>
+
+      </TouchableOpacity>
+    </View>
+  );
+}
+    
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    allignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#FFFFE0',
+  },
+
+  button: {
+    
+    backgroundColor: "#778899",
+    padding: 20,
+    borderRadius: 5
+
+  },
+
+  insts: {
+    fontSize: 18,
+    color: "#87CEFA",
+    marginHorizontal: 15,
+    marginBottom: 10,
+
+  },
+
+  logo: {
+    width: 310,
+    height: 300,
+    marginBottom: 20
+  },
+  
+  buttonText: {
+    fontSize: 20,
+    color: "#fff"
+  }
+
+});
